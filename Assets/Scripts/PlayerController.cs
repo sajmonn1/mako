35e099104a8833a4b0c32556c4f8a43f6080563e
Szymon Ignatiuk <szymon.ignatiuk@teb.edu.pl>
2024-02-28 13:13:19 +0100

    cos

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,27 +4,72 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //silnik fizyczny dla obiektu gracza
+    Rigidbody rb;
+    //si³a skoku
+    public float jumpForce = 5f;
+
     public float moveSpeed = 5f;
-        // Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
-        
+        //przypnij rigidbody gracza do zmiennej rb
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //transform.position += new Vector3(1, 0, 0) * Time.deltaTime;
-        float x = Input.GetAxis("Horizontal");
-        Debug.Log(x);
+        //mo¿na proœciej: Vector3.right
 
-        Vector3 movement = Vector3.right * x;
+        //zczytaj klawiaturê w osi poziomej:
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        //wyœwietl w konsoli stan klawiatury
+        Debug.Log(horizontalInput);
 
-        float y = Input.GetAxis("Vertical");
-        movement += Vector3.forward * y;
+        //wylicz przesuniêcie w osi x
+        Vector3 movement = Vector3.right * horizontalInput;
+
+        //zczytaj z klawiatury oœ y
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        //wylicz przesuniêcie w osi y i dodaj do istniej¹cego przesuniêcia w osi x
+        movement += Vector3.forward * verticalInput;
+
+        //normalizujemy wektor
         movement = movement.normalized;
+        //poprawka na czas od ostatniej klatki
         movement *= Time.deltaTime;
+        //pomnó¿ przez prêdkoœæ ruchu
         movement *= moveSpeed;
-        transform.position += movement;
+
+        //przesuñ gracza w osi x
+        //transform.position += movement;
+
+        //próbujemy u¿yc translate zamiast dodawac wspó³rzêdne
+        transform.Translate(movement);
+
+
+    }
+    //spróbujmy obejœæ problem z opóŸnieniem wejœcia poprzez przeniesienie go do update
+    void Update()
+    {
+        //sprawdz czy nacisnieto spacjê (skok)
+        //zwraca true jeœli zaczêliœmy naciskaæ spacjê w trakcie klatki animacji
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        //sprawdz czy znajduje siê na poziomie 0
+        if (transform.position.y <= Mathf.Epsilon)
+        {
+            //dodaj si³ê skoku
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
     }
 }
